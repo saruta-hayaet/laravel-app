7a2019caae318f7fc66147b51a960124df5561a8
saruta-hayaet <sarumilk0301@gmail.com>
2023-05-25 06:40:12 +0900

    adminテーブル作成

--- database/migrations/2023_05_24_212103_create_admins_table.php
+++ database/migrations/2023_05_24_212103_create_admins_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -22,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('admins');
+        Schema::dropIfExists('users');
     }
 };
